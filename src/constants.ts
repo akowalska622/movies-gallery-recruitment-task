88375490ae978758afa5b799ef39d988937eedf8
Ania Kowalska <a.kowalska622@gmail.com>
2024-07-23 17:29:09 +0200

    feat: add fetching movies by genre

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const GENRE_ID = {
+  ADVENTURE: 12,
+  ANIMATION: 16,
+  DOCUMENTARY: 99,
+} as const;
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,33 @@
-import Config from 'react-native-config';
+import {GENRE_ID} from '../constants';
 
-export const fetchCharacters = async (page: number = 1) => {
-  const response = await fetch(
-    `https://rickandmortyapi.com/api/character?page=${page}`,
-  );
+type GenreId = (typeof GENRE_ID)[keyof typeof GENRE_ID];
+
+export const fetchFromTMDB = async (
+  endpoint: string,
+  params: Record<string, string | number>,
+) => {
+  const url = new URL(endpoint, process.env.TMDB_BASE_URL);
+
+  url.searchParams.append('api_key', process.env.TMDB_API_KEY ?? '');
+
+  for (const [key, value] of Object.entries(params)) {
+    url.searchParams.append(key, String(value));
+  }
+  const parsedUrl = url.toString();
+
+  const response = await fetch(parsedUrl);
+
+  if (!response.ok) {
+    throw new Error('Network issues');
+  }
+
   return await response.json();
+};
+
+// Returning only 1st page for the sake of simplicity in challenge
+export const fetchMoviesByGenre = async (genreId: GenreId) => {
+  const response = await fetchFromTMDB('discover/movie', {
+    with_genres: genreId,
+  });
+  return response.results;
 };
