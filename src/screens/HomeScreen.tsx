9422ab1bdb52ab123449a6d4c89398d5ee8a6d32
Ania Kowalska <a.kowalska622@gmail.com>
2024-07-23 18:17:46 +0200

    feat: prepare fetchMovieById service

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,27 +1,13 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {View, Text, Button} from 'react-native';
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
 
 import type {RootStackParamList} from '../types';
 import {APP_ROUTES} from '../routes';
-import {fetchMoviesByGenre} from '../services/api';
-import {GENRE_ID} from '../constants';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
 export const HomeScreen = ({navigation}: Props) => {
-  useEffect(() => {
-    const loadMovies = async () => {
-      try {
-        const movies = await fetchMoviesByGenre(GENRE_ID.ADVENTURE);
-        console.log({movies});
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    loadMovies();
-  }, []);
   return (
     <View>
       <Text>Main screen</Text>
--- src/services/api.ts
+++ src/services/api.ts
@@ -4,14 +4,16 @@ type GenreId = (typeof GENRE_ID)[keyof typeof GENRE_ID];
 
 export const fetchFromTMDB = async (
   endpoint: string,
-  params: Record<string, string | number>,
+  params?: Record<string, string | number>,
 ) => {
   const url = new URL(endpoint, process.env.TMDB_BASE_URL);
 
   url.searchParams.append('api_key', process.env.TMDB_API_KEY ?? '');
 
-  for (const [key, value] of Object.entries(params)) {
-    url.searchParams.append(key, String(value));
+  if (params) {
+    for (const [key, value] of Object.entries(params)) {
+      url.searchParams.append(key, String(value));
+    }
   }
   const parsedUrl = url.toString();
 
@@ -25,9 +27,16 @@ export const fetchFromTMDB = async (
 };
 
 // Returning only 1st page for the sake of simplicity in challenge
+// otherwise would use pagination
 export const fetchMoviesByGenre = async (genreId: GenreId) => {
   const response = await fetchFromTMDB('discover/movie', {
     with_genres: genreId,
   });
   return response.results;
+};
+
+export const fetchMovieById = async (movieId: number) => {
+  const response = await fetchFromTMDB(`movie/${movieId}`);
+
+  return response;
 };
